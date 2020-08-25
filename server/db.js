f54f89346111969499f4db9c0ae7bb833ec40026
benz0is <62246716+benz0is@users.noreply.github.com>
2020-08-25 15:39:17 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,6 +1,19 @@
 const { Pool } = require("pg");
 const pool = new Pool();
 
+pool.query(
+  "CREATE TABLE IF NOT EXISTS valiutos ( id BIGSERIAL NOT NULL PRIMARY KEY, currency TEXT NOT NULL UNIQUE, rate TEXT NOT NULL)",
+  (req, res) => {
+    console.log(req, res);
+  }
+);
+pool.query(
+  "CREATE TABLE IF NOT EXISTS history(id BIGSERIAL NOT NULL PRIMARY KEY,from_num TEXT NOT NULL,to_cur TEXT NOT NULL)",
+  (req, res) => {
+    console.log(req, res);
+  }
+);
+
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
